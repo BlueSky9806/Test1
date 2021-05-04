8e7b5b6f62fb93bf8462bbd333dce57eb5f3964e
andy <andy@qq.com>
2021-05-04 14:44:58 +0800

    hot-fix 分支第一次提交

--- src/main/java/com/hao/git/GitDemo.java
+++ src/main/java/com/hao/git/GitDemo.java
@@ -6,5 +6,6 @@ public class GitDemo {
         System.out.println("Hello Git!");
         System.out.println("Hello Git2222222");
         System.out.println("Hello Git3333333");
+        System.out.println("hot-fix");
     }
 }
