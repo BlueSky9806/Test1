2a2070a70555376d94e88422f2e7ba342ca79f16
andy <andy@qq.com>
2021-05-04 13:54:14 +0800

    第一次修改后提交

--- src/main/java/com/hao/git/GitDemo.java
+++ src/main/java/com/hao/git/GitDemo.java
@@ -4,5 +4,7 @@ public class GitDemo {
 
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git2222222");
+        System.out.println("Hello Git3333333");
     }
 }
